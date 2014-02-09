0bc4648ebf02809de9b749dad9724036e955b483
Amine TheDream <atourki@gmail.com>
2014-02-08 19:32:00 -0500

    building rails app

---
+++ step/dayone/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dayone::Application.config.secret_token = 'b663dea199dbcb54171e1c6fa2924a65feb061ff3ed4fefd5efcf1a4cc88085c3d757528f2d00139433f6d574f66c05d20c048384932c2ccad1275dcbc74dcc8'
